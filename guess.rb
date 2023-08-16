6df734e6828d63cc368a371d959a445268529477
Sbhboi <142192418+Sbhboi@users.noreply.github.com>
2023-08-16 11:42:48 +0800

    adding more function

--- guess.rb
+++ guess.rb
@@ -1,23 +1,48 @@
-puts "Roll the Dice !"
-secret_number = 59
-puts secret_number
+scoring_card = {}
 
-counter = 1
-chances = 6
-guesses = []
+loop do
 
-while counter <= chances do
+	puts "Roll the Dice !"
+	secret_number = 59
+	puts "Enter your Name"
+	player_name = gets.chomp
+	puts "Hello #{player_name}"
+	#puts secret_number
+	puts "Lets Roll ~!"
 
-	answer = gets.chomp.to_i
+	counter = 1
+	chances = 6
+	guesses = []
 
-	if answer > secret_number
-		puts "Whelp, too high"
-	elsif answer < secret_number
-		puts "Noope, too low"
-	else
-		puts "You are correct!"
-		break
+	while counter <= chances do
+
+		answer = gets.to_i
+
+			if answer > secret_number
+				puts "Whelp, too high"
+			elsif answer < secret_number
+				puts "Noope, too low"
+			else
+				puts "You are correct!"
+				break
+			end
+		
+		guesses << answer
+		puts "guesses: #{guesses}"
+		counter +=1
 	end
-	
+
+	puts "Quick Guesser !"
+	scoring_card[player_name] =counter
+	scoring_card.sort_by {|player,score| score}.each do |player, score|
+		puts "#{player}: #{score}"
+	end
+
+	puts "Continue?"
+	puts "Y - Yes"
+	puts "N - No"
+
+	play = gets.chomp.downcase
+	break if play != "y"
 
 end
