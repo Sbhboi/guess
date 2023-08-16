725eef13d2a02875bc40efb0dccc98185f7451ce
Sbhboi <142192418+Sbhboi@users.noreply.github.com>
2023-08-16 15:55:54 +0800

    Cleanup

--- guess.rb
+++ guess.rb
@@ -1,49 +1,66 @@
-scoring_card = {}
+def set_default_value
+	@secret_number = 59
+	@counter = 1
+	@chances = 6
+	@guesses = []
+end
+
+def set_name
+	puts "Roll the Dice !"
+	puts "Enter your Name"
+	@player_name = gets.chomp
+	puts "Hello #{@player_name}"
+	#puts @secret_number
+	puts "Lets Roll ~!"
+end
+
+def set_score_guess
+	puts "Quick Guesser !"
+	@scoring_card[@player_name] =@counter
+	@scoring_card.sort_by {|player,score| score}.each do |player, score|
+		puts "#{player}: #{score}"
+	end
+end
+
+def set_continue_play
+	puts "Continue?"
+	puts "Y - Yes"
+	puts "N - No"
+end
+
+@scoring_card = {}
 
 loop do
 
-	puts "Roll the Dice !"
-	secret_number = 59
-	puts "Enter your Name"
-	player_name = gets.chomp
-	puts "Hello #{player_name}"
-	#puts secret_number
-	puts "Lets Roll ~!"
+	set_default_value
+	set_name
 
-	counter = 1
-	chances = 6
-	guesses = []
-
-	while counter <= chances do
+	while @counter <= @chances do
 
 		answer = gets.to_i
 
-			if answer > secret_number
+			if answer > @secret_number
 				puts "Whelp, too high"
-			elsif answer < secret_number
+			elsif answer < @secret_number
 				puts "Noope, too low"
 			else
 				puts "You are correct!"
 				break
 			end
 		
-		guesses << answer
-		puts "guesses: #{guesses}"
-		counter +=1
+		@guesses << answer
+		puts "@guesses: #{@guesses}"
+		@counter +=1
 	end
 
-	puts "Quick Guesser !"
-	scoring_card[player_name] =counter
-	scoring_card.sort_by {|player,score| score}.each do |player, score|
-		puts "#{player}: #{score}"
-	end
+	set_score_guess
 
 	puts "Continue?"
 	puts "Y - Yes"
 	puts "N - No"
 
 	play = gets.chomp.downcase
-	break if play != "y"
+	break if play != "y" 
  
 end
 
